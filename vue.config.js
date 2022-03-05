444e88ab7c1e8c4244a8f2c565d7e5717f8b9915
lx <coderlx@163.com>
2022-03-05 10:12:17 +0800

    初始化项目

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     assetsDir: "static",
     devServer: {
         proxy: {
-            'api/v1': {
+            '/api/v1/': {
               target: 'http://127.0.0.1:9999',
               changeOrigin: true,
             }
